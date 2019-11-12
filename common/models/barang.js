9e7c965ab09d33e7e964b4ee27303940c8b0312b
farrasavia <farras251096@gmail.com>
2019-11-12 14:06:48 +0700

    add remote methodbykategori

--- common/models/barang.js
+++ common/models/barang.js
@@ -20,6 +20,25 @@ module.exports = function(Barang) {
           callback(err);
         });
       };
+      Barang.getBarangByKategori = function(kategori, callback) {
+        new Promise(function(resolve, reject) {
+                // find name
+          Barang.find({where: {kategori: {like: kategori}}}, function(err, result) {
+            if (err) reject(err);
+            if (result === null) {
+              err = new Error('User not Found');
+              err.statusCode = 404;
+              reject(err);
+            }
+            resolve(result);
+          });
+        }).then(function(res) {
+          if (!res) callback(err);
+          return callback(null, res);
+        }).catch(function(err) {
+          callback(err);
+        });
+      };
     Barang.remoteMethod(
         'getBarangByNama',
     {
@@ -32,6 +51,21 @@ module.exports = function(Barang) {
       },
       http: {path: '/getBarangByNama', verb: 'get'},
     }
-    );  
+    );
+    Barang.remoteMethod(
+      'getBarangByKategori',
+  {
+    description: 'get barang by kategori',
+    accepts: [
+              {arg: 'kategori', type: 'string'},
+    ],
+    returns: {
+      arg: 'res', type: 'object', root: true,
+    },
+    http: {path: '/getBarangByKategori', verb: 'get'},
+  }
+  );
+    
+    
 
 };
